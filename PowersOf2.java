056ac3b067f064b862e153c5563cbb67f366077d
Carl Piercy <cpiercy111@gmail.com>
2017-10-20 10:00:14 -0700

    10/20

--- PowersOf2.java
+++ PowersOf2.java
@@ -14,7 +14,7 @@ public class PowersOf2
             int numPowersOf2;        //How many powers of 2 to compute
             int nextPowerOf2 = 1;        //Current power of  2
             int exponent;                //Exponent for current power of 2 -- this
-                                         //also serves as a counter for the loop
+            int printedPower;                             //also serves as a counter for the loop
             Scanner scan = new Scanner(System.in);
  
             System.out.println("How many powers of 2 would you like printed?");
@@ -22,13 +22,14 @@ public class PowersOf2
             exponent = 1;   
             //print a message saying how many powers of 2 will be printed
             //initialize exponent -- the first thing printed is 2 to the what?
- 
-            while (exponent < numPowersOf2)
+            printedPower = 1;
+            System.out.println(printedPower);
+            for (exponent = 1; exponent < numPowersOf2; exponent++)
             {
                 //print out current power of 2
- 
+                printedPower = 2*printedPower;
                 //find next power of 2 -- how do you get this from the last one?
- 
+                System.out.println(printedPower);
                 //increment exponent
  
             }
