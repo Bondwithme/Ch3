92f8988de36d34c747c02e4b0b0e69befb35987e
Carl Piercy <cpiercy111@gmail.com>
2017-10-25 09:56:56 -0700

    10/25

--- multiTable.java
+++ multiTable.java
@@ -1,5 +1,13 @@
 
 public class multiTable
 {
-
+    public static void main(String [] args)
+    {
+        for(int i = 1; i <= 12; i++) {
+            for(int b = 1; b <= 12; b++) {
+                System.out.print(b*i + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
---
+++ starProgram.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class starProgram
+{
+    public static void main(String[] args) {
+    //Unfinished
+    int inputStars;
+    Scanner scan = new Scanner(System.in);
+    inputStars = scan.nextInt();
+    for(int i = 0; i<inputStars; i++) {
+        System.out.print("*");
+    }
+   }
+}
